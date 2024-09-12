1ead5bc7c5ca14b4c01ce9fc4a6166b2ccc2f44a
IamAshish2 <karkiaashish9899@gmail.com>
2024-09-12 23:04:34 +0545

    added comments for better understanding of recursive way of reversing singly linked list

--- src/LinkedList/simpleProblems/RawLL.java
+++ src/LinkedList/simpleProblems/RawLL.java
@@ -1,16 +1,17 @@
 package LinkedList.simpleProblems;
 
 public class RawLL {
-     Node head;
-    RawLL(){
+    Node head;
+
+    RawLL() {
         this.head = null;
     }
 
-    class Node{
+    class Node {
         int data;
         Node next;
 
-        Node(int data){
+        Node(int data) {
             this.data = data;
             this.next = null;
         }
@@ -26,32 +27,32 @@ public class RawLL {
         ll.addFirst(2);
         System.out.println("index of 7 is " + ll.indexOf(7));
         ll.iterate();
-//        ll.head = ll.reverseRecursive(ll.head);
+        ll.head = ll.reverseRecursive(ll.head);
         System.out.println();
         ll.iterate();
     }
 
     // find index of a value in linked list
-    int indexOf(int data){
+    int indexOf(int data) {
         Node currNode = head;
         int idx = 0;
-        while (currNode.next != null){
+        while (currNode.next != null) {
             idx++;
-            if(currNode.data == data){
-                return idx -1;
+            if (currNode.data == data) {
+                return idx - 1;
             }
             currNode = currNode.next;
         }
         return 0;
     }
 
-    boolean isEmpty(){
+    boolean isEmpty() {
         return head == null;
     }
 
-    void addFirst(int data){
+    void addFirst(int data) {
         Node newNode = new Node(data);
-        if(isEmpty()){
+        if (isEmpty()) {
             head = newNode;
 //            System.out.println(head.data);
             return;
@@ -60,22 +61,22 @@ public class RawLL {
         head = newNode;
     }
 
-    void addLast(int data){
+    void addLast(int data) {
         Node newNode = new Node(data);
-        if(isEmpty()){
+        if (isEmpty()) {
             head = newNode;
             return;
         }
 
         Node currNode = head;
-        while (currNode.next != null){
+        while (currNode.next != null) {
             currNode = currNode.next;
         }
         currNode.next = newNode;
     }
 
-    int deleteFirst(){
-        if(isEmpty()){
+    int deleteFirst() {
+        if (isEmpty()) {
             return -1;
         }
 
@@ -84,12 +85,12 @@ public class RawLL {
         return temp.data;
     }
 
-    int deleteLast(){
-        if(isEmpty()){
+    int deleteLast() {
+        if (isEmpty()) {
             return -1;
         }
 
-        if(head.next == null){
+        if (head.next == null) {
             Node last = head;
             head = null;
             return last.data;
@@ -98,7 +99,7 @@ public class RawLL {
         Node secondLastNode = head;
         Node lastNode = head.next;
 
-        while (lastNode.next != null){
+        while (lastNode.next != null) {
             lastNode = lastNode.next;
             secondLastNode = secondLastNode.next;
         }
@@ -106,13 +107,13 @@ public class RawLL {
         return lastNode.data;
     }
 
-    void iterate(){
+    void iterate() {
         Node currNode = head;
-        if(isEmpty()){
+        if (isEmpty()) {
             System.out.println("list is empty");
             return;
         }
-        while (currNode != null){
+        while (currNode != null) {
             System.out.print(currNode.data + "->");
             currNode = currNode.next;
         }
@@ -120,15 +121,15 @@ public class RawLL {
     }
 
     // reverse a linked list iterative method
-    void reverseIterate(){
-        if(head == null || head.next == null){
+    void reverseIterate() {
+        if (head == null || head.next == null) {
             return;
         }
 
         Node prevNode = head;
         Node currNode = head.next;
 
-        while (currNode != null){
+        while (currNode != null) {
             Node nextNode = currNode.next;
             currNode.next = prevNode;
 
@@ -140,15 +141,35 @@ public class RawLL {
         head.next = null;
         head = prevNode;
     }
-    // reverse a linked list recursive method
-    Node reverseRecursive(Node head){
-        if(head == null || head.next == null){
-            return  head;
-        }
 
+    // reverse a linked list recursive method
+    Node reverseRecursive(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        // recursive calls to create that backward pointing linked list and assign the last node as the new head
         Node newHead = reverseRecursive(head.next);
+
+        // we assume as we go up the stack, that for every node , it is the head
+        // -> so, say for 2 (2->3->4->null) we assume 2 to be it's head
+        // so when we return from recursive call with (head->4->3->) and our initial
+        // linked list going up the stack looks like (2->3->4->null)
+        // here, since we assumed that 2 is going to be our head in this level
+        // if we do, head.next i.e it is now pointing towards 3 and head.next.next = head i.e. now pointing
+        // towards the head of current upward recursion process -> itself(head)
+        // meaning ----> with this operation the rightward node is pointing towards leftward node
+        // something like this now->>>>>>> head->4->3->2
         head.next.next = head;
+
+        // now what this is doing is, since we successfully pointed rightward node towards leftward node which is in reverse direction
+        // we can now break the connection we originally had with our connection node previously.
+        // before (1->2->3->4->null)  and (head->4->3) where 2 (our head at this point going upward) is pointing
+        // toward 3 and 3 towards 2 after head.next.next action
+        //after head.next (head->4->3->2->) and we break the connection with 3. and what remains is (1->2)
         head.next = null;
+
+        // at last we return our new head i.e. the last node of the previous node
+        // happy reversing ✅✅✅✅✅
         return newHead;
     }
 }
