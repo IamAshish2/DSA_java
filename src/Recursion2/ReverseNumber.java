9d0950c498a5f6b2d2be4d6640992c2fadbd033d
IamAshish2 <karkiaashish9899@gmail.com>
2024-10-11 21:12:49 +0545

    reverse a number

--- src/Recursion2/ReverseNumber.java
+++ src/Recursion2/ReverseNumber.java
@@ -2,17 +2,10 @@ package Recursion2;
 
 public class ReverseNumber {
     public static void main(String[] args) {
-        System.out.println(reverse2(4321));
+        System.out.println(reverse(1234));
     }
 
-    static int reverse(int n){
-        if (n % 10 == n){
-            return  n;
-        }
-
-        return  10 * (reverse((n%10)) + reverse(n/10));
-    }
-
+    /*
     static int reverse2(int num) {
         int sum = 0;
         while (num != 0){
@@ -22,6 +15,19 @@ public class ReverseNumber {
         }
         return sum;
     }
+     */
 
+    // recursive method to reverse a number
+    public static int reverse(int num){
+        // the Math.log10(num) will return the digits = actual number of digits - 1 i.e. 0 indexed
+        int digits = (int) Math.log10(num);
+        return  helper(num,digits);
+    }
 
+    public static int helper(int num,int digits){
+        if(num % 10 == num){
+            return num;
+        }
+        return  num % 10 * (int) Math.pow(10,digits) + helper(num/10,digits-1);
+    }
 }
