942063e1cbefec9d1bd19f03d12f0269252da54b
IamAshish2 <karkiaashish9899@gmail.com>
2024-10-15 22:20:53 +0545

    right angle traingle

--- src/Recursion2/Refresher/RightAnglePattern.java
+++ src/Recursion2/Refresher/RightAnglePattern.java
@@ -6,7 +6,6 @@ public class RightAnglePattern {
     }
 
     static void printTraingle(int row,int col){
-
         if (row == 0){
             return;
         }
@@ -14,9 +13,23 @@ public class RightAnglePattern {
             System.out.print("* ");
             printTraingle(row,col+1);
         }
-        if (col == row){
+        if (col > row){
             System.out.println();
             printTraingle(row-1,1);
+        }
+    }
+
+    static void printTraingle2(int row, int col){
+        if (row == 0){
+            return;
+        }
+        if (col <= row){
+            printTraingle2(row,col+1);
+            System.out.print("* ");
+        }
+        if (col > row){
+            printTraingle2(row-1,1);
+            System.out.println();
         }
     }
 }
